36d25ad22ac629067f1bbc5da15e4b9899ce5443
Walloef <wallof.kristofer@gmail.com>
2022-03-03 00:54:30 +0100

    Playing with react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,43 @@
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../src/firebase/client";
 import { useAuth } from "../src/providers/AuthUserContext";
 
 export default function Home() {
   const { authUser, logOut } = useAuth();
 
+  const docData = {
+    stringExample: "Hello world!",
+    booleanExample: true,
+    numberExample: 3.14159265,
+    arrayExample: [5, true, "hello"],
+    nullExample: null,
+    objectExample: {
+      a: 5,
+      b: {
+        nested: "foo",
+      },
+    },
+  };
+
+  // const test = async () => {
+  //   console.log("hej");
+  //   // await setDoc(doc(db, "data", "one"), docData);
+  // };
+  const test = async () => {
+    await setDoc(doc(db, "data", "one"), docData);
+  };
   console.log(authUser);
   return (
     <div>
       {authUser && (
-        <button type="button" onClick={logOut}>
-          Sign out
-        </button>
+        <>
+          <button type="button" onClick={logOut}>
+            Sign out
+          </button>
+          <button type="button" onClick={test}>
+            DB TEST
+          </button>
+        </>
       )}
       hi
     </div>
--- pages/italiano/index.tsx
+++ pages/italiano/index.tsx
@@ -1,19 +1,24 @@
 import { useAuth } from "../../src/providers/AuthUserContext";
 import { db } from "../../src/firebase/client";
-import { addDoc, collection } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { FormEvent, useState } from "react";
-import { parseUrl } from "next/dist/shared/lib/router/utils/parse-url";
 
 const verbConjugations = ["jag", "du", "vi", "han", "hon", "dom"];
 
-const url =
-  "https://translate.google.com/?sl=sv&tl=it&text=Jag%20%C3%A4ter%2C%20du%20%C3%A4ter%2C%20vi%20%C3%A4ter%2C%20de%20%C3%A4ter%2C%20hon%20%C3%A4ter%2C%20han%20%C3%A4ter&op=translate";
+const UI_STATES = {
+  ADD_SWEDISH_VERB: 1,
+  OPEN_TRANSLATE: 2,
+  ADD_TRANSLATE_URI: 3,
+};
 
 export default function Italiano() {
   const [word, setWord] = useState("");
   const [googleUri, setGoogleUri] = useState("");
   const [viewGoogleUri, setViewGoogleUri] = useState(false);
   const [newGoogleUri, setNewGoogleUri] = useState("");
+  const [currentUiState, setCurrentUiState] = useState(
+    UI_STATES.ADD_SWEDISH_VERB
+  );
 
   const { authUser } = useAuth();
 
@@ -21,88 +26,83 @@ export default function Italiano() {
     e.preventDefault();
     let verbs = "";
     verbConjugations.forEach((verb) => {
-      console.log(verb);
       verbs = ` ${verbs} ${verb} ${word},`;
     });
 
     const encodedUri = encodeURI(
-      `https://translate.google.com/?sl=sv&tl=it&text=${verbs.trimStart()}&op=translate`
+      `https://translate.google.com/?sl=sv&tl=it&text=${verbs
+        .trimStart()
+        .trimEnd()}&op=translate`
     );
     setGoogleUri(encodedUri);
-    // console.log(googleUri);
   };
 
   const writeToFirestore = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const newUri = new URL(newGoogleUri).search.split("text=")[1].split("&")[0];
-    // console.log(decodeURIComponent(newGoogleUri));
-
+    const verbs: string[] = [];
     decodeURIComponent(newUri)
       .split(",")
-      .map((verb) => {
-        if (verb.trimStart()) {
-          // console.log(verb.trimStart());
-          const key = verb.trimStart().split(" ")[0].trim();
-          const value = verb.trimStart().split(" ")[1].trim();
-          console.log(key, value);
-          addDoc(collection(db, "verbs", word), {
-            key: value,
-          });
-          // addDoc(collection(db, "verbs"), {
-          //   text: "hej",
-          //   verb.trimStart(): "hej"
-          // })
-          //   .then((docRef) => {
-          //     console.log(docRef);
-          //   })
-          //   .catch((error) => console.log(error));
+      .forEach((verb) => {
+        if (verb) {
+          verbs.push(verb.trim());
         }
       });
-
-    console.log(newGoogleUri);
-
-    return;
-    addDoc(collection(db, "verbs"), {
-      text: "hej",
+    await setDoc(doc(db, "verbs", word), {
+      verbs,
     })
-      .then((docRef) => {
-        console.log(docRef);
+      .then((res) => {
+        console.log(res);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
-      {googleUri && (
-        <a
-          href={googleUri}
-          target="_blank"
-          rel="noreferrer"
-          onClick={() => setViewGoogleUri(true)}
-        >
-          Google translate
-        </a>
-      )}
-      {/* {googleUri} */}
-      {/* <input type="text" />
-      <button></button> */}
-      {/* <button onClick={writeToFirestore}>hejhej</button> */}
-      {authUser &&
-        (!viewGoogleUri ? (
-          <form onSubmit={(e) => getGoogleUri(e)}>
-            <input type="text" onChange={(e) => setWord(e.target.value)} />
-            <button type="submit">Open google translate</button>
+      {authUser && (
+        <>
+          <form
+            onSubmit={(e) => {
+              getGoogleUri(e);
+              setCurrentUiState(2);
+            }}
+          >
+            <fieldset disabled={currentUiState !== 1}>
+              <label htmlFor="verb"></label>
+              <input
+                id="verb"
+                type="text"
+                onChange={(e) => setWord(e.target.value)}
+              />
+              <button type="submit">Open google translate</button>
+            </fieldset>
           </form>
-        ) : (
+          <div className={currentUiState === 2 && "active"}>
+            <a
+              href={googleUri}
+              target="_blank"
+              rel="noreferrer"
+              onClick={() => {
+                setViewGoogleUri(true);
+                setCurrentUiState(3);
+              }}
+            >
+              Google translate
+            </a>
+          </div>
           <form onSubmit={(e) => writeToFirestore(e)}>
-            <input
-              type="text"
-              onChange={(e) => setNewGoogleUri(e.target.value)}
-            />
-            <button type="submit">add Google translate uri</button>
+            <fieldset disabled={currentUiState !== 3}>
+              <input
+                type="text"
+                onChange={(e) => setNewGoogleUri(e.target.value)}
+              />
+              <button type="submit">add Google translate uri</button>
+            </fieldset>
           </form>
-        ))}
-      hi
+        </>
+      )}
     </div>
   );
 }
